eb44f3557a8f9c3cfb905d3fbeffac08bf3df031
Ni30kr91 <nitish.kumar8092@gmail.com>
2022-06-13 18:01:44 +0530

    done

--- leap.go
+++ leap.go
@@ -6,7 +6,7 @@ func main() {
 
 	var yr int
 	fmt.Println("Enter the year:")
-	fmt.Scanf("%d", &yr)
+	fmt.Scanln(&yr)
 	if yr%4 == 0 && yr%100 != 0 || yr%400 == 0 {
 		fmt.Println("The year is a leap year")
 	} else {
